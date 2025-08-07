52bea4740388e11e93227092fd7f2d4b15b2ba0f
raj-iitr <r_vinayak@mt.iitr.ac.in>
2025-08-07 21:58:44 +0530

    detector added

--- app/detector.py
+++ app/detector.py
@@ -2,8 +2,7 @@ from langdetect import detect
 
 def detect_lang(text: str) -> str:
     try:
-        logger.info(f"Detecting language for text: {text}")
-        print(f"Detected language: {lang}")
+        lang = detect(text)
         return lang if lang in ["hi", "en"] else "hi-en"
     except:
         return "unknown"
